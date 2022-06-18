07ea269ddbbaae716df50823987fae5399f18e1d
marialshy <marialshy@gmail.om>
2022-06-18 12:28:54 +0300

    added branch check-program - first version to compare

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// для исходного массива сформировать новый, где каждый элемент не длиннее 3х символов
+Console.WriteLine("Please enter symbols splited by comma");
+string someInput = Convert.ToString(Console.ReadLine());
+int number = 3;
+
+string [] newArray = FillArrayIfChar3(someInput);
+PrintArray(newArray);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+string [] FillArrayIfChar3(string text)
+{
+    int j = 0;
+    string[] array = text.Split(',');
+    string[] arrayNew = new string [array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (Check3Char(array[i], number))
+        {
+            arrayNew[j] = array[i];
+            j++;
+        }
+    }
+    return arrayNew;
+}
+
+bool Check3Char(string charA, int number)
+{
+    if (charA.Length <= number) return true;
+    else return false;
+}
+
