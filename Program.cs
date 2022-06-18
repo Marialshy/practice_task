0c82c607c0d863a27c9e15baf8d7ad8cb072bddb
marialshy <marialshy@gmail.om>
2022-06-18 12:20:50 +0300

    changed main method: convert string to array out

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// to create new array (string), each item's length <=3
+Console.WriteLine("Please enter symbols splited by comma");
+string someInput = Convert.ToString(Console.ReadLine());
+int number = 3;
+string[] arrayA = someInput.Split(',');
+string [] newArray = FillArrayIfChar3(arrayA);
+PrintArray(newArray);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+string [] FillArrayIfChar3(string [] array)
+{
+    int j = 0;
+    string[] arrayNew = new string [array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (Check3Char(array[i], number))
+        {
+            arrayNew[j] = array[i];
+            j++;
+        }
+    }
+    return arrayNew;
+}
+
+bool Check3Char(string charA, int number)
+{
+    if (charA.Length <= number) return true;
+    else return false;
+}
